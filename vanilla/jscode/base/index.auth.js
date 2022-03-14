da378e75a7f87144b9e50401423194d0875c1640
nikolkovjec <98321417+nikolkovjec@users.noreply.github.com>
2022-03-14 12:39:06 +0100

    A working material welcome page

---
+++ vanilla/jscode/base/index.auth.js
@@ -0,0 +1,159 @@
+(function() {
+  'use strict';
+
+angular.module('web')
+//.service('auth', authService)
+.controller('LoginController', LoginController)
+.controller('LogoutController', LogoutController)
+
+.config(function($authProvider) {
+
+	$authProvider.loginUrl =
+        window.location.protocol + "//"
+        + window.location.host
+        + "/auth";
+	$authProvider.tokenName = 'authentication_token';
+
+	$authProvider.oauth1({
+		  name: null,
+		  url: null,
+		  authorizationEndpoint: null,
+		  redirectUri: null,
+		  type: null,
+		  popupOptions: null
+	});
+
+})
+;
+
+/* NOTE TO SELF:
+A quick note to make login/logout work in combination with flask:
+
+JS works wherever you want, while python pages has to be really loaded
+to make python code work on server side.
+
+To make this possible you have to use buttons,
+were you make the browser go to URLs which are not intercepted by angular router.
+
+As i found out here:
+http://stackoverflow.com/a/25799503
+there is a quick paragraph in angular docs to make this happen:
+https://docs.angularjs.org/guide/$location
+
+"In cases like the following, links are not rewritten; 
+instead, the browser will perform a full page reload to the original link.
+Links that contain target element
+Example: <a href="/ext/link?a=b" target="_self">link</a>
+Absolute links that go to a different domain
+Example: <a href="http://angularjs.org/">link</a>
+Links starting with '/' that lead to a different base path
+Example: <a href="/not-my-base/link">link</a>"
+
+So for login i can make the page reload, for instance 
+(do not like much for now)
+While for logout i made the button "Yes" to let it happen. 
+
+
+*/
+
+//////////////////////////////
+function LoginController($scope, $window, $location, $log, $auth, $state, $timeout) {
+
+    $log.debug("Login Controller");
+    $scope.load = true;
+    $timeout(function() {
+        var token = $auth.getToken();
+        $log.debug("Actual token is:", token);
+        if (token !== null) {
+            $state.go('logged');
+        } else {
+            $scope.load = false;
+        }
+    }, 1000);
+
+    $scope.loginfun = function(credentials) {
+        $log.debug("Requested with", credentials);
+
+        $auth.login(credentials).then(
+            function (loginResponse) {
+                console.log(loginResponse);
+                console.log($auth.getToken());
+/////////////////////////////////////////
+// THERE IS NO WAY TO MAKE THIS TWO IN ONE COMMAND...
+                //$state.go('logged');
+                $window.location.reload();
+/////////////////////////////////////////
+
+            }, function(errorResponse) {
+                $log.warn("Failed");
+                console.log(errorResponse.data.errors);
+
+            }
+            );
+    }
+}
+
+function LogoutController($scope, $log, $auth) 
+{
+    $log.debug("Logout Controller");
+
+    $scope.logoutfun = function() {
+
+        console.log("Logging out");
+    	$auth.logout().then(function() {
+            console.log("Token cleaned:", $auth.getToken());
+        });
+    }
+
+    // auth.authenticatedRequest(token)
+    //  .then(function logged(some){
+    //     console.log("Token in storage is:", auth.getToken());
+    // });
+
+}
+
+// THE END
+})();
+
+/*
+///////////////////////////////////////////
+// https://thinkster.io/angularjs-jwt-auth
+///////////////////////////////////////////
+function authService($window, $http, $log) {
+
+    var self = this;
+    var FE_URL = 'http://awesome.dev'
+    var API_URL = 'http://awesome.dev:8081/api'
+
+////////////
+    self.saveToken = function(token) {
+      $window.localStorage['jwtToken'] = token;
+      return token;
+    }
+    self.getToken = function() {
+      return $window.localStorage['jwtToken'];
+    }
+////////////
+
+    self.requestToken = function(content) {
+        var req = {
+            method: 'POST',
+            url: FE_URL + '/auth',
+            // headers: {
+            //   'Content-Type': undefined
+            // },
+            data: { 'username': content.email, 'password': content.pwd }
+        }
+
+        return $http(req).then(
+            function successCallback(response) {
+                $log.info("Authentication successful");
+                return self.saveToken(response.data.user.authentication_token);
+          }, function errorCallback(response) {
+                $log.error("Authentication FAILED")
+                //console.log(response);
+                return self.saveToken(null);
+        });
+    }
+}
+*/
--- vanilla/jscode/base/index.config.js
+++ vanilla/jscode/base/index.config.js
@@ -1,129 +1,155 @@
 (function() {
   'use strict';
 
-angular.module('web')
-    //.config(config_modules)
-    .config(config);
+angular.module('web').config(config);
 
-/* NG ROUTER
-function config($routeProvider, $locationProvider, $logProvider)
-{
-    $locationProvider.html5Mode(true);
-    $logProvider.debugEnabled(true); //.hashPrefix('!');
-    console.log("TEST 1");
-    $routeProvider
-        .when('/test', {
-            templateUrl: '/static/app/templates/home.html',
-            controller: 'MainController'
-        })
-        .otherwise({ redirectTo: '/test' });
-    console.log("TEST 2");
-}
-*/
-
-/* LAZY LOADING
-function config_modules($ocLazyLoadProvider) {
-
-    var mylib =
-        "http://awesome.dev" +
-        "/static/bower/angular-loading-bar/build/loading-bar.min.js"
-
-    $ocLazyLoadProvider.config({
-      modules: [{
-        name: 'cfp.loadingBar',
-        files: [mylib],
-      }]
-    });
-
-//$ocLazyLoadProvider.load('cfp.loadingBar');
-
-}
-*/
-
-function config(
-        $logProvider, $locationProvider, $interpolateProvider,
-        $stateProvider, $urlRouterProvider)
-    {
-    	// Enable log
-    	$logProvider.debugEnabled(true); //.hashPrefix('!');
-        // HTML5 mode: remove hash bang to let url be parsable
-        $locationProvider.html5Mode(true);
-        // Change angular variables from {{}} to [[]]
-        //$interpolateProvider.startSymbol('[[').endSymbol(']]');
-
-        $stateProvider
-
-/////////////////////////////////////////////////////////
-// THIS IS USED DIRECTLY BY PYTHON?
-            //.state('welcome', { url: "/helloworld", })
-// DOES NOT WORK...
-
-/////////////////////////////////////////////////////////
-// TRUE JS PAGES FROM HERE BELOW
-
-            ////////////////////////////
-            .state("login", {
-                url: "/login",
-                 views: {
-                    "main": {
-                        //template: '<br><h1>test</h1> [[angular]]',
-                        templateUrl: '/static/app/templates/login.html',
-                        controller: 'LoginController',
-                    }
-                }
-            })
-            .state("logout", {
-                url: "/logout",
-                 views: {
-                    "main": {
-                        templateUrl: '/static/app/templates/logout.html',
-                        controller: 'LoginController',
-                    }
-                }
-            })
-            .state("logged", {
-                url: "/app"
-                // Abstract state?
-            })
-
-            .state('logged.searh', {
-                url: "^/search",
-                views: {
-                    "main": {
-                        templateUrl: '/static/app/templates/home.html',
-                        controller: 'MainController',
-                    }
-                },
-            })
-
+// Ui Resolve + Satellizer to authenticate
+// source http://j.mp/1VnxlQS
+function _skipIfAuthenticated($q, $state, $auth) {
+    var defer = $q.defer();
+    //console.log("STATE", $state);
 /*
-            ////////////////////////////
+    if($auth.isAuthenticated()) {
+        defer.reject();
+    } else {
+        defer.resolve();
+    }
+*/
+        defer.resolve();
+    return defer.promise;
+}
+ 
+function _redirectIfNotAuthenticated($q, $state, $auth, $timeout) {
+    var defer = $q.defer();
+    if($auth.isAuthenticated()) {
+        defer.resolve();
+    } else {
+        //console.log("TEST");
+        $timeout(function () {
+            //console.log("TEST2");
+            $state.go('login');
+        }); //, 100);
+        defer.reject();
+    }
+    return defer.promise;
+}
 
-            .state("data", {
-                url: "/data",
-                views: {
-                    "main": {
-                        template: 'Welcome to data page <div ui-view="id"></div>',
-                        controller: 'DataController',
+// ROUTES
+function config($stateProvider, $urlRouterProvider, $authProvider, $logProvider, $locationProvider) //, $interpolateProvider)
+{
 
-                    }
+	// Enable log
+	$logProvider.debugEnabled(true); //.hashPrefix('!');
+    // HTML5 mode: remove hash bang to let url be parsable
+    $locationProvider.html5Mode(true);
+    // Change angular variables from {{}} to [[]]
+    //$interpolateProvider.startSymbol('[[').endSymbol(']]');
+
+$stateProvider
+    ////////////////////////////
+
+    .state("login", {
+        url: "/login",
+        resolve: {
+            skipIfAuthenticated: _skipIfAuthenticated
+        },
+        views: {
+            "main": {
+                //template: '<br><h1>test</h1> [[angular]]',
+                templateUrl: '/static/app/templates/login.html',
+                controller: 'LoginController',
+            }
+        }
+    })
+    .state("logged", {
+        url: "/app",
+        resolve: {
+            redirectIfNotAuthenticated: _redirectIfNotAuthenticated
+            //logginer: function($auth) {
+        },
+        // Implement main route for landing page after login
+        views: {
+            "menu": {
+                templateUrl: 'static/app/templates/menu.html',
+                controller: 'AppRootController',
+            },
+            "main": {
+        // and add a child view called 'loggedview' for logged pages
+                templateUrl: 'static/app/templates/logged.html',
+                //controller: 'AppRootController',
+            }
+        },
+    })
+
+    .state('logged.submit', {
+        url: "/submit",
+        views: {
+            "loggedview": {
+                templateUrl: '/static/app/templates/home.html',
+                controller: 'MainController',
+            }
+        },
+    })
+
+    .state('logged.search', {
+        url: "/search",
+        views: {
+            "loggedview": {
+                templateUrl: '/static/app/templates/home.html',
+                controller: 'MainController',
+            }
+        },
+    })
+
+    .state("logged.logout", {
+        url: "/logout",
+        views: {
+            "loggedview": {
+                templateUrl: '/static/app/templates/logout.html',
+                controller: 'LogoutController',
+            }
+        }
+    })
+
+    // Routes definition end
+    ;
+
+//$urlRouterProvider.otherwise('/login');
+
+// Ui router kinda bug fixing 
+// CHECK THIS IN THE NEAR FUTURE
+//https://github.com/angular-ui/ui-router/issues/1022#issuecomment-50628789
+    $urlRouterProvider.otherwise(function ($injector) {
+        var $state = $injector.get('$state');
+        return $state.go('login');
+    });
+/* 
+    // In case you want to redirect to external link
+    $urlRouterProvider.otherwise(function () {
+        window.location.href = '/';
+        //$location.path('/helloworld');
+        //$location.replace();
+    });
+*/
+/*
+    .state("hello", {
+        url: "/helloworld",
+        resolve: {
+            skipIfAuthenticated: _skipIfAuthenticated
+        },
+        views: {
+            "main": {
+                controller: function($window) {
+                    //Reload python pages
+                    console.log("Reload!");
+                    $window.location.reload();
                 }
-                })
-           .state("data.id", {
-                url: "/:id",
-                views: {
-                    "id": {
-                        template: '<h1>id = [[id]]</h1>',
-                        controller: 'DataIDController',
-
-                    }
-                },
-            });
-
+            }
+        }
+    })
 */
 
+}
 
-        $urlRouterProvider.otherwise('/login');
-    }
 
 })();
